c04b6b35e1d7122c8380cd502048274a196f056c
Hjelt Touko EXT <touko.hjelt@digia.com>
2020-03-25 15:06:31 +0200

    add more info and hilight numbers

--- handlers/statsHandler.js
+++ handlers/statsHandler.js
@@ -9,6 +9,7 @@ const _ = require('underscore');
 const _botNotificationName = 'hoitsubotti';
 
 var _treshold = moment().add(-1, 'day');
+const _perHourTimeWindow = moment().add(-2, 'days');
 
 /**
  * Gets last operation run time
@@ -102,12 +103,13 @@ function _createCaseData(operation, confirmedCases, deadCases, recoveredCases) {
 
     var lastUpdateString = _getLatestOperationTime(operation).add(2, 'hours').format('DD.MM.YYYY HH:mm');
     var confirmedNew = _.filter(confirmedCases, function (c) { return c.acqDate.isAfter(_treshold); });
+    var confirmedPerHour = (_.filter(confirmedCases, function (c) { return c.acqDate.isAfter(_perHourTimeWindow); }).length / 48).toFixed(1);
     var recoveredNew = _.filter(recoveredCases, function (c) { return c.date.isAfter(_treshold); });
     var deadNew = _.filter(deadCases, function (c) { return c.date.isAfter(_treshold); });
     var confirmedPercent = (confirmedNew.length / (confirmedCases.length || 1) * 100).toFixed(0);
-    var ingress = `Tartuntoja ${confirmedCases.length}, joista 24h aikana ${confirmedNew.length}.\nKasvua ${confirmedPercent}% vuorokaudessa.\n\nParantuneita ${recoveredCases.length}, joista 24h aikana ${recoveredNew.length}.`;
+    var ingress = `Tartuntoja <strong>${confirmedCases.length}</strong>, joista 24h aikana <strong>${confirmedNew.length}</strong>.\nKasvua <strong>${confirmedPercent}</strong>% vuorokaudessa.\n\n<strong>${confirmedPerHour}</strong> uutta tartuntaa tunnissa viimeisen 48h aikana.\n\nParantuneita <strong>${recoveredCases.length}</strong>, joista 24h aikana <strong>${recoveredNew.length}</strong>.`;
 
-    if (deadCases.length) ingress += `\n\nKuolleita ${deadCases.length}, joista 24h aikana ${deadNew.length}.`;
+    if (deadCases.length) ingress += `\n\nKuolleita <strong>${deadCases.length}</strong>, joista 24h aikana <strong>${deadNew.length}</strong>.`;
 
     var resultMsg = helper.formatListMessage(`Tilastot (${lastUpdateString})`, ingress, [], []);
     var confirmedDataString = _getCaseDataTableString(confirmedCases, confirmedCols, 'acqDate', _treshold);
